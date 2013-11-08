4366884eb1299aec822d77c6a28130075215fbcd
Zach <zach@bluehousegroup.com>
2013-11-07 19:42:34 -0500

    calculations done

--- Expressions_Personal/js/problemset.js
+++ Expressions_Personal/js/problemset.js
@@ -5,8 +5,16 @@ WPF - Assignment: Expressions
 */
 
 //problem: how much is it going to cost me to drive to work this week?
+
+//prompt for days worked per week
 var workDaysPerWeek = parseInt(prompt('How many days per week do you work?'));
-var commuteDistance = parseInt(prompt('How many miles is your commute to work?'));
+//prompt for distance to work - multiply by two so we can calculate roundtrip
+var commuteDistance = parseInt(prompt('How many miles is your commute to work (one way)?'))*2;
+//prompt for milage per gallon of vehicle
 var milesPerGallon = parseInt(prompt('How many miles per gallon does your vehicle get?'));
 
+//national average 11/7/13
 var priceGallonGas = 3.222;
+
+var totalGallonsNeeded = ((commuteDistance*workDaysPerWeek)/milesPerGallon);
+var totalPriceOfGallonsNeeded = totalGallonsNeeded*priceGallonGas;
