76d6a08599b3017cad0704df863ae81a8779090a
Zach <zach@bluehousegroup.com>
2013-11-07 20:16:49 -0500

    finished calc for industry problem

--- Expressions_Industry/js/problemset.js
+++ Expressions_Industry/js/problemset.js
@@ -6,10 +6,23 @@ WPF - Assignment: Expressions
 
 //Problem: given a skill and number of hours practiced, how many hours until you are technically a master?
 
+//get name of skill and remove whitespace
 var skill = prompt('What skill do you practice to be good at your job?  For example - if you are a web developer, a skill might be "javascript"').trim;
+//get number of years practiced
 var yearsPracticed = parseInt(prompt('How many years have you been actively practicing the skill (numeric)?'));
+//get number of hours practiced per week
 var avgHoursWeek = parseInt(prompt('How many hours per week do you dedicate to practicing (numeric)?'));
 
 //they say that to be considered a master, you need to have practiced for ten thousand hours
 var reqHoursForMastery = 10000;
 
+//calc hours practiced by calc hours practiced per year multiplied by number of years
+var hoursPracticed = (avgHoursWeek*52)*yearsPracticed;
+//get num of hours remaining until mastery
+var timeRemaining = reqHoursForMastery - hoursPracticed;
+
+//if time remaining then output, else congradulate
+var catString = (timeRemaining > 0) ? 'You have '+timeRemaining+' hours left of practice until you are considered a master.  Go get em!' : 'YOU ARE A MASTER';
+
+//log
+console.log(catString);
