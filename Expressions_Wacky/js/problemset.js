349eb42e6f237f056b65e5c9d904a7cb43c50242
Zach <zach@bluehousegroup.com>
2013-11-07 21:09:32 -0500

    finished with wacky problem

--- Expressions_Wacky/js/problemset.js
+++ Expressions_Wacky/js/problemset.js
@@ -12,7 +12,7 @@ var favDesserts = prompt('List three favorite desserts, separated by commas');
 
 //convert strings into arrays
 favFoods = favFoods.split(',');
-favDrinks = favFoods.split(',');
+favDrinks = favDrinks.split(',');
 favDesserts = favDesserts.split(',');
 
 //check to see if user entered three comma separated items
@@ -20,26 +20,22 @@ if(favFoods.length != 3 || favDrinks.length != 3 || favDesserts.length != 3){
 	alert('Please enter three of each item separated by commas');
 }
 
-//attach functionality to javascript array
-Array.prototype.getRandomFavorite = function(){
-	//use math.rand and array.length to select a random index (getting the floor so we know we have a whole number)
-	return this[Math.floor(Math.random() * this.length)]
-}
-
 //create varable to pour parings into
-var pairingSuggestions;
+var pairingSuggestions = new Array();
 
+//create three suggestions
 for(var i = 0; i < 3; i++){
-	//use our new function to select a random item from array
-	var randomFood = favFoods.getRandomFavorite;
-	var randomDrink = favDrinks.getRandomFavorite;
-	var randomDessert = favDesserts.getRandomFavorite;
-	//remove item from array so don't randomly select the same element
-	favFoods.push(randomFood);
-	favDrinks.push(randomDrink);
-	favDesserts.push(randomDessert);
+	//use math.rand and array.length to select a random index (floor so we know we have a whole number)
+	var randomFood = favFoods[Math.floor(Math.random() * favFoods.length)];
+	var randomDrink = favDrinks[Math.floor(Math.random() * favDrinks.length)];
+	var randomDessert = favDesserts[Math.floor(Math.random() * favDesserts.length)];
+	//remove item from array so we don't randomly select the same element
+	favFoods.splice(favFoods.indexOf(randomFood), 1);
+	favDrinks.splice(favDrinks.indexOf(randomDrink), 1);
+	favDesserts.splice(favDesserts.indexOf(randomDessert), 1);
 	//concatinate
-	pairingSuggestions[] = randomFood+' + '+randomDrink+' + '+randomDessert;
+	pairingSuggestions.push(randomFood+' + '+randomDrink+' + '+randomDessert);
 }
 
+//log
 console.log('Our expert panel recommends the following pairings: ('+pairingSuggestions[0]+'), ('+pairingSuggestions[1]+'), ('+pairingSuggestions[2]+')');
