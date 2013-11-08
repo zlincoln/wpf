0ddf8e85b0382381aa46732756497896ff2e7618
Zach <zach@bluehousegroup.com>
2013-11-07 19:36:32 -0500

    expression_personal checkpoint

---
+++ Expressions_Personal/js/problemset.js
@@ -0,0 +1,12 @@
+/*
+Zachary Lincoln
+WPF - Assignment: Expressions
+11/7/13
+*/
+
+//problem: how much is it going to cost me to drive to work this week?
+var workDaysPerWeek = parseInt(prompt('How many days per week do you work?'));
+var commuteDistance = parseInt(prompt('How many miles is your commute to work?'));
+var milesPerGallon = parseInt(prompt('How many miles per gallon does your vehicle get?'));
+
+var priceGallonGas = 3.222;
