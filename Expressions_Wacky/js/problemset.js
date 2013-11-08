0a25b1674eee93f10abd717e5626eae000551f62
Zach <zach@bluehousegroup.com>
2013-11-07 20:49:51 -0500

    almost finished with wacky problem

--- Expressions_Wacky/js/problemset.js
+++ Expressions_Wacky/js/problemset.js
@@ -6,6 +6,34 @@ WPF - Assignment: Expressions
 
 //problem: given a list of favorite foods, drinks, and desserts, make random pairings
 
-var favFoods;
-var favDrinks;
-var favDesserts;
+var favFoods = prompt('List three favorite foods, separated by commas');
+var favDrinks = prompt('List three favorite drinks, separated by commas');
+var favDesserts = prompt('List three favorite desserts, separated by commas');
+
+//convert strings into arrays
+favFoods = favFoods.split(',');
+favDrinks = favFoods.split(',');
+favDesserts = favDesserts.split(',');
+
+//check to see if user entered three comma separated items
+if(favFoods.length != 3 || favDrinks.length != 3 || favDesserts.length != 3){
+	alert('Please enter three of each item separated by commas');
+}
+
+//attach functionality to javascript array
+Array.prototype.getRandomFavorite = function(){
+	return this[Math.floor(Math.random() * this.length)]
+}
+
+//create varable to pour parings into
+var pairingSuggestions;
+
+for(var i = 0; i < 3; i++){
+	var randomFood = favFoods.getRandomFavorite;
+	favFoods.push(randomFood);
+	var randomDrink = favDrinks.getRandomFavorite;
+	favDrinks.push(randomDrink);
+	var randomDessert = favDesserts.getRandomFavorite;
+	favDesserts.push(randomDessert);
+	pairingSuggestions[] = randomFood+' + '+randomDrink+' + '+randomDessert;
+}
