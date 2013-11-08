2dd89199503e2ce4543d6f14a89d1947f22dc8d6
Zach <zach@bluehousegroup.com>
2013-11-07 20:32:26 -0500

    starting third problem

---
+++ Expressions_Wacky/js/problemset.js
@@ -0,0 +1,11 @@
+/*
+Zachary Lincoln
+WPF - Assignment: Expressions
+11/7/13
+*/
+
+//problem: given a list of favorite foods, drinks, and desserts, make random pairings
+
+var favFoods;
+var favDrinks;
+var favDesserts;
