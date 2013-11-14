4ef1bb20c0e98f53e610518687ca0884e75e3b66
Zach <zach@bluehousegroup.com>
2013-11-14 01:00:19 -0500

    progress checkpoint

--- Conditionals_Industry/js/problemset.js
+++ Conditionals_Industry/js/problemset.js
@@ -4,3 +4,42 @@ WPF - Assignment: Conditionals
 11/13/13
 */
 
+//problem: given pixel values of static mockup, return calculated percentages in css format for responsive markup
+
+function validateSelectorPrompt(promptText, errorText){
+	errorText = (typeof errorText === 'undefined') ? '' : errorText;
+	thePrompt = prompt(errorText+promptText);
+	if(thePrompt == ''){
+		validateSelectorPrompt(promptText, 'Please provide some input.  ');
+	}else if(typeof thePrompt != 'string'){
+		validateSelectorPrompt(promptText, 'Please provide plain text input.  ');
+	}else{
+		return thePrompt;
+	}
+}
+
+var parentSelectorName = validateSelectorPrompt('Please provide a selector for the parent element.');
+
+var targetSelectorName = validateSelectorPrompt('Please provide a selector for the target element');
+
+function validateValuePrompt(promptText, errorText){
+	errorText = (typeof errorText === 'undefined') ? '' : errorText;
+	thePrompt = parseInt(prompt(errorText+promptText));
+	if(thePrompt == ''){
+		validateSelectorPrompt(promptText, 'Please provide some input.  ');
+	}else if(typeof thePrompt != 'number'){
+		validateSelectorPrompt(promptText, 'Please provide numeric input.  ');
+	}else{
+		return thePrompt;
+	}
+}
+
+var parentPixelWidth = validateValuePrompt('What is the pixel width of the parent element?');
+
+var targetPixelWidth = validateValuePrompt('What is the pixel width of the target element?');
+
+function validatePaddingMarginPrompt(){
+	
+}
+
+var targetPaddingMargin = validatePaddingMarginPrompt();
