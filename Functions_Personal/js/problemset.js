6d9f1c4812cacf730ab5d8b30c6b7fe2067130de
Zach <zach@bluehousegroup.com>
2013-11-21 20:59:17 -0500

    finished with js - need testing and comments

--- Functions_Personal/js/problemset.js
+++ Functions_Personal/js/problemset.js
@@ -32,17 +32,38 @@ function numberPrompt(promptText, errorText){
 }
 
 var Password = {
+	"wordString": '',
 	"randSpecialChar": ['!','*','%','$'],
 	simple: function(wordArray, number){
-		wordArray.sort(function(){return 0.5 - Math.random()});
-		return wordArray.pop()+wordArray.pop()+wordArray.pop()+wordArray.pop()+number;
+		if(this.wordString == ''){
+			wordArray.sort(function(){return 0.5 - Math.random()});
+			for(var i = 0; i < 4; i++){
+				var passPiece = wordArray.pop();
+				this.wordString += passPiece.trim();
+			}
+		}
+		return this.wordString+number;
 	},
 	maxSecurity: function(wordArray, number){
-		
+		if(this.wordString == ''){
+			wordArray.sort(function(){return 0.5 - Math.random()});
+			for(var i = 0; i < 4; i++){
+				var passPiece = wordArray.pop();
+				this.wordString += passPiece.trim();
+			}
+		}
+		this.randSpecialChar.sort(function(){return 0.5 - Math.random()});
+		return this.wordString.charAt(0).toUppercase()+this.wordString.slice(1)+number+this.randSpecialChar.pop();
 	}
 }
 
 var passPartsArray = promptForPassParts('In a comma separated list, can you provide a list of words that have relevance to you?  Please provide at least four.');
 
 var luckyNumber = numberPrompt('What\'s your lucky number?');
+
+var userPass = new Password();
+
+console.log(userPass.simple(passPartsArray,luckyNumber));
+console.log(userPass.maxSecurity(passPartsArray,luckyNumber));
+
 
