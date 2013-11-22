1e68b711247ceccfb3654e67005188083ba2b2d0
Zach <zach@bluehousegroup.com>
2013-11-21 23:33:48 -0500

    complete work - needs testing

--- Functions_Wacky/js/problemset.js
+++ Functions_Wacky/js/problemset.js
@@ -9,10 +9,21 @@ WPF - Assignment: Functions
 function numberPrompt(promptText, errorText){
 	errorText = (typeof errorText == 'undefined') ? '' : errorText;
 
-	var theNumber = prompt(errorText+promptText);
+	var theNumber = parseInt(prompt(errorText+promptText));
 
 	if(isNaN(theNumber)){
 		numberPrompt(promptText, 'Please provide numeric input.  ');
+	}else{
+		return theNumber;
 	}
-
 }
+
+var birthDate = numberPrompt('Whats your birthdate?  Please format in MMDDYYYY without separators.');
+
+var favNumber = numberPrompt('Whats your favorite number?');
+
+var firstNum = numberPrompt('Whats the first number that jumps into your head?');
+
+var magicNumber = Math.floor((birthDate*favNumber)/firstNum);
+
+console.log('Your special magic number is:  '+magicNumber.toString());
