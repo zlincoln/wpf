04059c14da534e648960361810fcdbc17db38161
Zach <zach@bluehousegroup.com>
2013-11-21 23:35:01 -0500

    halfway done commenting

--- Functions_Wacky/js/problemset.js
+++ Functions_Wacky/js/problemset.js
@@ -6,12 +6,17 @@ WPF - Assignment: Functions
 
 //get the magic number
 
+//create number prompt validation function
 function numberPrompt(promptText, errorText){
+	//fallback for error text
 	errorText = (typeof errorText == 'undefined') ? '' : errorText;
 
+	//get the number
 	var theNumber = parseInt(prompt(errorText+promptText));
 
+	//if no number
 	if(isNaN(theNumber)){
+		//call the prompt again and explain why
 		numberPrompt(promptText, 'Please provide numeric input.  ');
 	}else{
 		return theNumber;
